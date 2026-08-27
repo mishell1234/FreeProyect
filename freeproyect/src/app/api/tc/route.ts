ec2e0b89bcc1e9abce4d8007c42fe8e071167799
Mishell Anthony Solis Marquez <solis_marks@hotmail.com>
2026-08-27 11:29:31 -0500

    feat: agregar endpoint de tipo de cambio

---
+++ freeproyect/src/app/api/tc/route.ts
@@ -0,0 +1,61 @@
+import { createClient } from '@supabase/supabase-js';
+import { NextResponse } from 'next/server';
+
+export async function GET() {
+  try {
+    const fecha = '2026-08-27';
+
+    const respuesta = await fetch(
+      `https://api.decolecta.com/v1/tipo-cambio/sunat?date=${fecha}`,
+      {
+        headers: {
+          Authorization: `Bearer ${process.env.KEY_API_SUNAT}`,
+        },
+      }
+    );
+
+    if (!respuesta.ok) {
+      return NextResponse.json(
+        { error: 'Error al consultar Decolecta' },
+        { status: respuesta.status }
+      );
+    }
+
+    const datos = await respuesta.json();
+
+    const tc = {
+      fecha: datos.date,
+      compra: Number(datos.buy_price),
+      venta: Number(datos.sell_price)
+    };
+
+    const supabase = createClient(
+      process.env.SUPABASE_URL!,
+      process.env.SUPABASE_ANON_KEY!
+    );
+
+    const { data, error} = await supabase
+      .from('tbl_tc')
+      .insert([tc])
+
+
+    if (error) {
+      console.error('Supabase error', error);
+
+      return NextResponse.json(
+        { error: 'Error al guardar el tipo de cambio'},
+        { status: 500}
+      );
+    }
+    
+    return NextResponse.json(tc);
+
+  } catch (error) {
+    console.error('Error:', error);
+
+    return NextResponse.json(
+      { error: 'Error inesperado del servidor' },
+      { status: 500 }
+    );
+  }
+}
